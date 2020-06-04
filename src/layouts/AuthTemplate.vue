1323ac5c06cfbb6e6655815cf9a6be9e8611202f
Clayton Figueiredo <claytongf@gmail.com>
2020-06-04 10:15:05 -0300

    First commit

---
+++ src/layouts/AuthTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <!-- Page Content -->
+        <div class="container container-body">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    @import url('../assets/css/reset.css');
+    @import url('../assets/css/auth.css');
+</style>
